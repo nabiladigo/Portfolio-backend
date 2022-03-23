88d8b0a4c3451d549cf3b70b5456c6745c344a9a
nabiladigo <nabiladigo91@gmail.com>
2022-03-22 20:41:40 -0400

    initialize project and adding database and server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(express.json()); // parse json bodies
+app.use(morgan('dev')); // logging
+
+
+
+app.get("/", (req,res) =>{
+    res.send("Hello World!");
+});
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
