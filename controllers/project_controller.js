3387a8404c6dd13dbb02bf4911413f6bcbe8046f
nabiladigo <nabiladigo91@gmail.com>
2022-03-24 12:20:18 -0400

    update server.js

--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const {Project} = require('../models');
 
 router.get('/', async (req, res) => {
+//    res.json(projects);
     try{
         res.json(await Project.find({}));
     } catch (err){
@@ -15,6 +16,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async(req, res)=>{
     try{
         res.json(await Project.findOne({'_id': req.params.id}));
+        
     }catch(err){
         res.status(400).json(err);
     };
@@ -23,10 +25,40 @@ router.get('/:id', async(req, res)=>{
 router.post('/', async(req, res)=>{
     try{
         res.json(await Project.create(req.body));
+
+        console.log("//////////////////////////////")
+        console.log("//////////////////////////////")
         console.log(req.body)
+        console.log("//////////////////////////////")
+        console.log("//////////////////////////////")
     }catch(err){
         res.status(400).json(err);
     };
+});
+
+router.put('/edit/:id', async(req, res)=>{
+    try{
+        const {name, image, live, git} = req.body;
+        const project = await Project.findByIdAndUpdate(req.params.id,
+            {
+                $set: req.body,
+            },
+            {
+                new: true,
+            }
+            );
+            const editProject = project.save();
+
+            console.log("//////////////////////////////")
+            console.log("//////////////////////////////")
+            console.log(editProject)
+            console.log("//////////////////////////////")
+            console.log("//////////////////////////////")
+
+            return res.status(200).json(editProject);
+    }catch(err){
+        return res.status(400).json(err)
+    }
 })
 
  module.exports = router;
--- server.js
+++ server.js
@@ -2,19 +2,50 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const controllers = require('./controllers');
+// const controllers = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const projects = require('./projects.json');
+const about =require('./about.json');
+const contact =require('./contact.json')
 
 app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(express.json()); // parse json bodies
 app.use(morgan('dev')); // logging
-app.use('/projects', controllers.project);
+// app.use('/projects', controllers.project);
 
 
-app.get("/", (req,res) =>{
-    res.send("Hello World!");
+
+//home route for testing our app
+app.get('/', (req,res) =>{
+    res.send('Hello World!');
 });
+
+// route for retrieving projects
+app.get('/projects', (req, res) =>{
+    // send projectes via JSON
+    res.json(projects);
+});
+app.get('/home', (req, res) =>{
+    // send projectes via JSON
+    res.json(homes);
+});
+
+
+app.get('/about', (req,res) =>{
+    
+    res.json(about);
+});
+app.get('/contact', (req,res) =>{
+    
+    res.json(contact);
+});
+app.get('/resume', (req,res) =>{
+    
+    res.json(resume);
+});
+
 
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
